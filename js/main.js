ba20eb3f5ba123476aca7768869f9b82128fdc34
Anthony Torrez <TonyT429@fullsail.edu>
2012-09-25 02:44:33 -0700

    making changes to main.js

--- js/main.js
+++ js/main.js
@@ -4,12 +4,28 @@ $('#home').on('pageinit', function(){
 		
 $('#addItem').on('pageinit', function(){
 
-		var myForm = $('#formId');
-		    myForm.validate({
-			invalidHandler: function(form, validator) {
-			},
-			submitHandler: function() {
-		var data = myForm.serializeArray();
+	var rbform = $('#recordbooksform'),
+		rberrorslink = $('#rberrorslink')
+	;
+	
+	rbform.validate({
+		invalidHandler: function(form, validator){
+			rberrorslink.click();
+			var html = '';
+			//console.log(validator.submitted);
+			for(var key in validator.submitted){
+				var label = $('label[for^="'+ key +'"]').not('[generated]');  // error with a label except those generated.
+				//console.log(label.text());
+				var legend = label.closest('fieldset').find('.ui-controlgroup-label');  
+				var fieldName = legend.length ? legend.text() : label.text();
+				console.log(fieldName);
+				html += '<li>'+ fieldName +'</li>';
+			};
+			$("#recordbookserrors ul").html(html);
+		},
+		submitHandler: function() {
+			var data = rbform.serializeArray();
+			localStorage.setItem('rbform', data);
 			storeData(data);
 		}
 	});
@@ -25,12 +41,37 @@ var autofillData = function (){
 };
 
 var getData = function(){
-
+	
 };
 
-var storeData = function(data){
+	// Get value of selected Radio Button
+	var radio = function() {
+		return $('input:radio[name=series]:checked').val();
+	};
 	
-}; 
+	
+var storeData = function(data){
+	if (!data) {
+		var id = Math.floor(Math.random()*1000001);
+	} else {
+		id = data;
+		
+		getSelectedRadio( ); // calls the value of the radio button
+		var item = { };
+			item.genre = [ "Genre:", $( '#genre' ).val() ];
+			item.title = [ "Book Title:", $( '#btitle' ).val() ];
+			item.author = [ "Author:", $( '#author' ).val() ];
+			item.isbn = [ "ISBN #:", $( '#isbn' ).val() ];
+			item.review = ["Comments:", $( '#review' ).val() ];
+			item.rate = ["Rate:",$( '#rate' ).val() ];
+			item.series = [ "Series:", series ]; // for radio buttons
+			item.seriesname = [ "Series Name:", $( '#seriesname' ).val() ];
+			item.seriesnum = [ "Series Number:", $( '#seriesnum' ).val() ];
+			item.date = [ "Date:", $( '#date' ).val() ];    // correcting missing date field
+			// Save data to local storage using JSON stringify to convert objects to a string.
+			localStorage.setItem( id, JSON.stringify( item ) );
+		alert ("Saved" ); 
+		}; 
 
 var	deleteItem = function (){
 			
